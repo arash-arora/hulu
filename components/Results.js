5b1a2a69b00f127223c6aa6fc3dd192da137d491
Arash Arora <52750629+arash-arora@users.noreply.github.com>
2021-07-16 02:17:55 +0530

    initial commit

---
+++ components/Results.js
@@ -0,0 +1,15 @@
+import requests from "../utils/requests";
+import Thumbnail from "./Thumbnail";
+
+function Results({ data }) {
+  console.log(data.results);
+  return (
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 grid-cols-3 3xl:flex flex-wrap justify-center'>
+      {data.results.map((data) => {
+        return <Thumbnail key={data.id} result={data} />;
+      })}
+    </div>
+  );
+}
+
+export default Results;
