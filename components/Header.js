d70c7aae708cf9a79ee9d1a919dbc1761c5703ef
Arash Arora <52750629+arash-arora@users.noreply.github.com>
2021-07-16 11:28:33 +0530

    links added

--- components/Header.js
+++ components/Header.js
@@ -9,16 +9,18 @@ import {
 } from "@heroicons/react/outline";
 import HeaderItem from "./HeaderItem";
 import logo from "./Arash.png";
-import Link from "next/link";
 
 function Header() {
   return (
     <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
       <div className='flex flex-grow justify-evenly max-w-2xl'>
-        <Link href='/'>
+        <a href='/'>
           <HeaderItem Icon={HomeIcon} title='HOME' />
-        </Link>
-        <HeaderItem Icon={LightningBoltIcon} title='TRENDING' />
+        </a>
+        <a href='/?genre=fetchTrending'>
+          <HeaderItem Icon={LightningBoltIcon} title='TRENDING' />
+        </a>
+        <a href=''></a>
         <HeaderItem Icon={BadgeCheckIcon} title='VERIFIED' />
         <HeaderItem Icon={CollectionIcon} title='COLLECTIONS' />
         <HeaderItem Icon={SearchIcon} title='SEARCH' />
